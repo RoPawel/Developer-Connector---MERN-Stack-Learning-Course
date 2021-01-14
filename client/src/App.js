7a0b813d682981843f5d4c871302790e173b6a16
propa <p.ropa@stigo.com.pl>
2021-01-14 15:50:46 +0100

    Added first components for Sign Up and Login with forms for user input

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Components/Layout/Navbar';
+import Landing from './Components/Layout/Landing';
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
+
+export default App;
