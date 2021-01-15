2aeafb0dfe1543b6d8dfcfe6347b38db52928f41
propa <p.ropa@stigo.com.pl>
2021-01-15 13:17:09 +0100

    Create Redux store, reducers, actions and their calls for Alert component

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,29 @@ import Navbar from './Components/Layout/Navbar';
 import Landing from './Components/Layout/Landing';
 import Register from './Components/auth/Register';
 import Login from './Components/auth/Login';
+import Alert from './Components/Layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
